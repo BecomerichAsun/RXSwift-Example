3e85cd289aa22832a4eac1475e577879a306236f
Asun <hongyang.zhong@uuabc.com>
2018-07-09 17:15:40 +0800

    1.提交 对combineLatest的总结 以及 combineLatest的拓展使用

--- Examples/Examples/combineLatest/ExamplesOneController.swift
+++ Examples/Examples/combineLatest/ExamplesOneController.swift
@@ -10,9 +10,16 @@ import UIKit
 import RxCocoa
 import RxSwift
 import NSObject_Rx
+/**
+ 总结:
+ 利用combineLatest特性,将三个输入框的字符串长度取出,做出判断,求和得出总结果
+ 
+ 拓展:
+此方法适用于 注册登录 输入框长度判断等模块
+ */
 
 class ExamplesOneController: UIViewController {
-  
+    
     @IBOutlet weak var threeTextField: UITextField!
     
     @IBOutlet weak var oneTextField: UITextField!
@@ -28,10 +35,12 @@ class ExamplesOneController: UIViewController {
         title = "combineLatest"
         view.backgroundColor = UIColor.white
         
-        sumBtn.addTarget(self, action: #selector(sum), for: .touchUpInside)
+        sumBtn.rx.tap
+            .subscribe(onNext: { [weak self] _ in self?.sum() })
+            .disposed(by: rx.disposeBag)
     }
     
-    @objc func sum() {
+    func sum() {
         Observable.combineLatest(oneTextField.rx.text.orEmpty, twoTextField.rx.text.orEmpty, threeTextField.rx.text.orEmpty) { textValue1, textValue2, textValue3 -> Int in
             return (Int(textValue1) ?? 0) + (Int(textValue2) ?? 0)+(Int(textValue3) ?? 0)
             }
@@ -39,3 +48,5 @@ class ExamplesOneController: UIViewController {
             .bind(to: result.rx.text).disposed(by: rx.disposeBag)
     }
 }
+
+
--- Examples/Examples/register/RegisterController.swift
+++ Examples/Examples/register/RegisterController.swift
@@ -10,6 +10,23 @@ import UIKit
 import RxSwift
 import RxCocoa
 import NSObject_Rx
+/**
+ 总结:
+ 界面布局就不多说了
+ 首先 这只是一个简单的登录注册逻辑
+ 逻辑判断:
+ 对两个输入框做出操作,先拿出输入框字符串长度做Map遍历操作,判断其长度 是否大于等于限定长度,尾部Share的添加是防止Map多次循环
+ SubjectLifetimeScope: 关于whileConnected这是默认值,当订阅者从1变成0时会被清除,作者推荐使用whileConnected
+ combineLatest:这个Examples中获取到帐号密码输入框输入的字符串长度,并且两个都不为空的时候,相当于一个并集
+ 
+ 数据绑定:
+ 将账号密码与提示文字所绑定bind(to:"code") Code位置可以填写弹窗等 (按照需求来)
+ 
+ 将账号密码的两个输入长度与按钮的启动做出绑定
+ 
+ 点击按钮:
+ 弹窗成功
+ */
 
 fileprivate let minimalUsernameLength = 5
 fileprivate let minimalPasswordLength = 5
@@ -29,10 +46,8 @@ class RegisterController: UIViewController {
         userTitle.text = "帐号不能小于\(minimalUsernameLength)位数"
         pwdTitle.text = "密码不能小于\(minimalPasswordLength)位数"
         
-        /**判断输入长度是否大于5*/
         let user = userInput.rx.text.orEmpty.map({$0.count >= minimalUsernameLength}).share(replay: 1, scope: SubjectLifetimeScope.whileConnected)
         let pwd = pwdInput.rx.text.orEmpty.map({$0.count >= minimalPasswordLength}).share(replay: 1, scope: SubjectLifetimeScope.whileConnected)
-        /**两个都成立的情况下*/
         let every = Observable.combineLatest(user,pwd) {$0 && $1}.share(replay: 1, scope: SubjectLifetimeScope.whileConnected)
         
         user.bind(to: userTitle.rx.isHidden).disposed(by: rx.disposeBag)
@@ -44,6 +59,7 @@ class RegisterController: UIViewController {
             .disposed(by: rx.disposeBag)
     }
 }
+
 
 extension RegisterController {
     func showAlert() {
